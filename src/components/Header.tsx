c313cfffc5bd6351645dfbb5aed228c47ed92d61
Albarral19 <jalbarral96@gmail.com>
2022-06-28 17:07:55 -0300

    feat: adicionado bg e main content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import { LogoPokemon } from "./LogoPokemon";
 
 export function Header() {
   return (
-    <header className="w-full h-[93px] bg-yellow-500 py-4 shadow-xl">      
-      <div className="container mx-auto flex items-center justify-between ">
+    <header className="w-full h-[93px] bg-yellow-500 py-4 shadow-xl">
+      <div className="container mx-auto px-24 flex items-center justify-between">
         <LogoPokemon />
-        <div className="text-black">
-          <a href="" className="p-8 font-medium text-2xl"><span className="border-black border-b-2 p-2">Home</span></a>
-          <a href="" className="p-8 font-medium text-2xl">Pokédex</a>
-          <a href="" className="p-8 font-medium text-2xl">Legendaries</a>
-          <a href="" className="p-8 font-medium text-2xl">Docs</a>
-        </div>        
+        <div className="font-medium text-2xl text-black">
+          <a href="" className="p-8"><span className="border-black border-b-2 p-2">Home</span></a>
+          <a href="" className="p-8">Pokédex</a>
+          <a href="" className="p-8">Legendaries</a>
+          <a href="" className="p-8">Docs</a>
+        </div>
       </div>
     </header>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
 import { Header } from "../components/Header";
+import imgHome from '../assets/pokemon-home.png'
 
 export function Home() {
   return (
-    <div className="home">
+    <div className="min-h-screen bg-yellow-500 bg-gradient-to-t from-yellow-600">
       <Header/>
-      <div>
-        <h1 className="text-7xl"><strong>Find</strong> all your favorite <strong>Pokemon</strong></h1>
-        <p className="text-3xl">You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
-        <button className="w-56 h-16 text-2xl font-semibold rounded-xl rounded-b-2xl border-b-[7px] border-emerald-500 bg-emerald-400">See pokemons</button>
-      </div>
-      
-      <div>
-
-      </div>
+      <main className="container mx-auto px-24 flex items-center justify-between">
+        <div className="pt-24 w-[516px]">
+          <h1 className="text-7xl tracking-widest"><strong>Find</strong> all your favorite <strong>Pokemon</strong></h1>
+          <p className="pt-16 pb-16 text-3xl">You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          <button 
+            className="w-56 h-16 text-2xl font-semibold rounded-xl rounded-b-2xl border-b-[7px] border-emerald-500 bg-emerald-400 hover:border-none">
+              See pokemons
+          </button>
+        </div>
+        
+        <div>
+          <img 
+            src={imgHome} 
+            alt="Pikachu"
+            className=" "
+        />
+        </div>
+      </main>      
     </div>
   )
 }
